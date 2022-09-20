cd0738bfa80c012fa955d12e098c6ef20634172f
eheath30 <elliot.heath@btinternet.com>
2022-09-20 23:59:33 +0100

    testing build

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -38,7 +38,7 @@ const Hero = () => {
       exit="exit"
     >
       <TitleContainer>
-        Let&#39;s talk about
+        <pre>Let&#39;s talk about</pre>
         <TypicalContainer>
           <Typical
             wrapper="p"
