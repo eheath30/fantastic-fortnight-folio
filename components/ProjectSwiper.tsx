9dc4d6e579fb55094ed81a4a594f130ee0d630b3
eheath30 <elliot.heath@btinternet.com>
2022-09-20 22:48:43 +0100

    creating projects section

--- components/ProjectSwiper.tsx
+++ components/ProjectSwiper.tsx
@@ -1,35 +1,233 @@
-import React from 'react'
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-creative";
 import { EffectCreative } from "swiper";
-import styles from "../styles/ProjectSwiper.module.css"
-const ProjectSwiper = () => {
-  return (
-  <Swiper
-    grabCursor={true}
-    effect={"creative"}
-    creativeEffect={{
-      prev: {
-        shadow: true,
-        translate: ["-125%", 0, -800],
-        rotate: [0, 0, -90],
-      },
-      next: {
-        shadow: true,
-        translate: ["125%", 0, -800],
-        rotate: [0, 0, 90],
-      },
-    }}
-    modules={[EffectCreative]}
-    className={styles.swiper}
-  >
-    <SwiperSlide><img src="https://cdn.vox-cdn.com/thumbor/3GvnfOvGZB3paInd57sdlY1Hvu8=/0x0:1006x1014/1200x0/filters:focal(0x0:1006x1014):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/22438217/Screen_Shot_2021_04_12_at_8.16.17_AM.png" width={"100%"} height={"auto"}></img></SwiperSlide>
-    <SwiperSlide><img src="https://cdn.vox-cdn.com/thumbor/3GvnfOvGZB3paInd57sdlY1Hvu8=/0x0:1006x1014/1200x0/filters:focal(0x0:1006x1014):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/22438217/Screen_Shot_2021_04_12_at_8.16.17_AM.png" width={"100%"} height={"auto"}></img></SwiperSlide>
-    <SwiperSlide><img src="https://cdn.vox-cdn.com/thumbor/3GvnfOvGZB3paInd57sdlY1Hvu8=/0x0:1006x1014/1200x0/filters:focal(0x0:1006x1014):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/22438217/Screen_Shot_2021_04_12_at_8.16.17_AM.png" width={"100%"} height={"auto"}></img></SwiperSlide>
-    <SwiperSlide><img src="https://cdn.vox-cdn.com/thumbor/3GvnfOvGZB3paInd57sdlY1Hvu8=/0x0:1006x1014/1200x0/filters:focal(0x0:1006x1014):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/22438217/Screen_Shot_2021_04_12_at_8.16.17_AM.png" width={"100%"} height={"auto"}></img></SwiperSlide>
-  </Swiper>
-  )
+import styles from "../styles/ProjectSwiper.module.css";
+import gamesgrotto1 from "../assets/ProjectsCarousel/gamesgrotto1.png";
+import gamesgrotto2 from "../assets/ProjectsCarousel/gamesgrotto2.png";
+import gamesgrotto3 from "../assets/ProjectsCarousel/gamesgrotto3.png";
+import gamesgrotto4 from "../assets/ProjectsCarousel/gamesgrotto4.png";
+import gamesgrotto5 from "../assets/ProjectsCarousel/gamesgrotto5.png";
+import habithole1 from "../assets/ProjectsCarousel/habithole1.png";
+import habithole2 from "../assets/ProjectsCarousel/habithole2.png";
+import habithole3 from "../assets/ProjectsCarousel/habithole3.png";
+import habithole4 from "../assets/ProjectsCarousel/habithole4.png";
+import habithole5 from "../assets/ProjectsCarousel/habithole5.png";
+import quizzlybears1 from "../assets/ProjectsCarousel/quizzlybears1.png";
+import quizzlybears2 from "../assets/ProjectsCarousel/quizzlybears2.png";
+import quizzlybears3 from "../assets/ProjectsCarousel/quizzlybears3.png";
+import quizzlybears4 from "../assets/ProjectsCarousel/quizzlybears4.png";
+import quizzlybears5 from "../assets/ProjectsCarousel/quizzlybears5.png";
+import portfolio1 from "../assets/ProjectsCarousel/portfolio1.png";
+import portfolio2 from "../assets/ProjectsCarousel/portfolio2.png";
+import portfolio3 from "../assets/ProjectsCarousel/portfolio3.png";
+
+interface INameProps {
+  name: string
 }
 
-export default ProjectSwiper
+const ProjectSwiper = (name: INameProps) => {
+  const ProjectImages = () => {
+    if (name.name === "Games Grotto") {
+      return (
+        <>
+          // TODO: https://www.img2go.com/ 934:895
+          <SwiperSlide>
+            <img
+              className={styles.projectimg}
+              src={gamesgrotto1.src}
+              width={"100%"}
+              height={"auto"}
+            ></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className={styles.projectimg}
+              src={gamesgrotto2.src}
+              width={"100%"}
+              height={"auto"}
+            ></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className={styles.projectimg}
+              src={gamesgrotto3.src}
+              width={"100%"}
+              height={"auto"}
+            ></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className={styles.projectimg}
+              src={gamesgrotto4.src}
+              width={"100%"}
+              height={"auto"}
+            ></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className={styles.projectimg}
+              src={gamesgrotto5.src}
+              width={"100%"}
+              height={"auto"}
+            ></img>
+          </SwiperSlide>
+        </>
+      );
+    }
+    else if (name.name === "Quizzly Bears") {
+      return (
+<>
+          // TODO: https://www.img2go.com/ 934:895
+          <SwiperSlide>
+            <img
+              className={styles.projectimg}
+              src={quizzlybears1.src}
+              width={"100%"}
+              height={"auto"}
+            ></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className={styles.projectimg}
+              src={quizzlybears2.src}
+              width={"100%"}
+              height={"auto"}
+            ></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className={styles.projectimg}
+              src={quizzlybears3.src}
+              width={"100%"}
+              height={"auto"}
+            ></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className={styles.projectimg}
+              src={quizzlybears4.src}
+              width={"100%"}
+              height={"auto"}
+            ></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className={styles.projectimg}
+              src={quizzlybears5.src}
+              width={"100%"}
+              height={"auto"}
+            ></img>
+          </SwiperSlide>
+        </>
+      )
+    }
+    else if (name.name === "Habit Hole") {
+      return (
+        <>
+          // TODO: https://www.img2go.com/ 934:895
+          <SwiperSlide>
+            <img
+              className={styles.projectimg}
+              src={habithole1.src}
+              width={"100%"}
+              height={"auto"}
+            ></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className={styles.projectimg}
+              src={habithole2.src}
+              width={"100%"}
+              height={"auto"}
+            ></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className={styles.projectimg}
+              src={habithole3.src}
+              width={"100%"}
+              height={"auto"}
+            ></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className={styles.projectimg}
+              src={habithole4.src}
+              width={"100%"}
+              height={"auto"}
+            ></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className={styles.projectimg}
+              src={habithole5.src}
+              width={"100%"}
+              height={"auto"}
+            ></img>
+          </SwiperSlide>
+        </>
+      );
+    }
+    else if (name.name === "Portfolio 2.0") {
+      return (
+        <>
+          // TODO: https://www.img2go.com/ 934:895
+          <SwiperSlide>
+            <img
+              className={styles.projectimg}
+              src={portfolio1.src}
+              alt={name.name}
+              width={"100%"}
+              height={"auto"}
+            ></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className={styles.projectimg}
+              src={portfolio2.src}
+              alt={name.name}
+              width={"100%"}
+              height={"auto"}
+            ></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className={styles.projectimg}
+              src={portfolio3.src}
+              alt={name.name}
+              width={"100%"}
+              height={"auto"}
+            ></img>
+          </SwiperSlide>
+        </>
+      )
+    }
+  };
+
+  return (
+    <Swiper
+      grabCursor={true}
+      effect={"creative"}
+      creativeEffect={{
+        prev: {
+          shadow: true,
+          translate: ["-125%", 0, -800],
+          rotate: [0, 0, -90],
+        },
+        next: {
+          shadow: true,
+          translate: ["125%", 0, -800],
+          rotate: [0, 0, 90],
+        },
+      }}
+      modules={[EffectCreative]}
+      className={styles.swiper}
+    >
+      {ProjectImages()}
+    </Swiper>
+  );
+};
+
+export default ProjectSwiper;
--- components/ProjectsGrid.tsx
+++ components/ProjectsGrid.tsx
@@ -1,76 +1,74 @@
 import React from "react";
-import ProjectSwiper from "./ProjectSwiper"
+import ProjectSwiper from "./ProjectSwiper";
 
 import {
   TitleContainer,
   ProjectsContainer,
   ProjectCard,
   ProjectsSection,
-  ProjectLinks
+  ProjectLinks,
 } from "../styles/ProjectsGrid.module.js";
 
+let projectdata = [
+  {
+    name: "Games Grotto",
+    description:
+      "The culmination of a full-stack training course. Built using JavaScript, ReactJS, Flask, PostgreSQL, Tiled, Bootstrap and custom CSS libararies.",
+    github: "https://github.com/eheath30/GamesGrotto-Client",
+    site: "https://games-grotto.netlify.app/",
+  },
+  {
+    name: "Quizzly Bears",
+    description:
+      "A full-stack online multiplayer quiz game. Built using reactjs, framermotion, bootstrap, socketio and redux",
+    github: "https://github.com/eheath30/quizzly-bears-netlify",
+    site: "https://quizzly-bears.netlify.app/",
+  },
+  {
+    name: "Habit Hole",
+    description:
+      "A sleep tracker with secure auth and data graphing. Built using html, css, javascript, docker, nodejs, expressjs, postgreSQL and chartjs",
+    github: "https://github.com/eheath30/GamesGrotto-Client",
+    site: "https://games-grotto.netlify.app/",
+  },
+  {
+    name: "Portfolio 2.0",
+    description:
+      "A full-stack portfolio site built using typescript, reactjs, styled components, nextjs and framer motion.",
+    github: "https://github.com/eheath30/GamesGrotto-Client",
+    site: "https://ehdev.netlify.app/",
+  },
+];
+
+export interface ListData {
+  name: string;
+  description: string;
+  github: string;
+  site: string;
+}
+
 const ProjectsGrid = () => {
+  const Projects = () => {
+    return projectdata.map(({ name, description, github, site }: ListData) => {
+      return (
+        <ProjectCard>
+          <h2>{name}</h2>
+          <p>{description}</p>
+          <ProjectSwiper name={name.toString()}/>
+          <ProjectLinks>
+            <a href={github}>Github</a>
+            <a href={site}>Live</a>
+          </ProjectLinks>
+        </ProjectCard>
+      );
+    });
+  };
+
   return (
     <ProjectsSection id="recent-projects">
       <TitleContainer>Projects</TitleContainer>
-      <ProjectsContainer>
-        <ProjectCard>
-          <h2>Games Grotto</h2>
-          <p>
-            The culmination of a full-stack training course. Built using JavaScript, ReactJS, Flask, PostgreSQL, Tiled, Bootstrap and custom CSS libararies.
-          </p>
-          <ProjectSwiper/>
-          <ProjectLinks>
-            <button>Github</button>
-            <button>Live</button>
-          </ProjectLinks>
-        </ProjectCard>
 
-        <ProjectCard>
-          <h2>header 2 second</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            corrupti temporibus vero repellendus quo ad, animi ullam reiciendis
-            laborum aliquid ut sequi qui commodi.
-          </p>
-        </ProjectCard>
-
-        <ProjectCard>
-          <h2>header 2 third</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            corrupti temporibus vero repellendus quo ad, animi ullam reiciendis
-            laborum aliquid ut sequi qui commodi.
-          </p>
-        </ProjectCard>
-
-        <ProjectCard>
-          <h2>header 2 fourth</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            corrupti temporibus vero repellendus quo ad, animi ullam reiciendis
-            laborum aliquid ut sequi qui commodi.
-          </p>
-        </ProjectCard>
-
-        <ProjectCard>
-          <h2>header 2 fifth</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            corrupti temporibus vero repellendus quo ad, animi ullam reiciendis
-            laborum aliquid ut sequi qui commodi.
-          </p>
-        </ProjectCard>
-
-        <ProjectCard>
-          <h2>header 2 sixth</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            corrupti temporibus vero repellendus quo ad, animi ullam reiciendis
-            laborum aliquid ut sequi qui commodi.
-          </p>
-        </ProjectCard>
-      </ProjectsContainer>
+      <ProjectsContainer>{Projects()}</ProjectsContainer>
     </ProjectsSection>
   );
 };
